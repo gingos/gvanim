7b225077b2f77dd15aba229c5849ccc4f1031cdb
yairschneid <yairschneid@gmail.com>
2016-08-10 13:01:13 +0300

    delete user control and added dbforn and sqlmethods

--- GvanimVS/GvanimVS/Coordinator.Designer.cs
+++ GvanimVS/GvanimVS/Coordinator.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace GvanimVS
 {
-    partial class UserControl1
+    partial class Coordinator
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,16 +20,26 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Coordinator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Coordinator";
+            this.ShowIcon = false;
+            this.Text = "Coordinator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
