034217a0714b685cbfb55498f4366192eb2d907b
yoadwo <yoadwo@ac.sce.ac.il>
2016-08-10 11:58:19 +0300

    delete Form1

--- GvanimVS/GvanimVS/Program.cs
+++ GvanimVS/GvanimVS/Program.cs
@@ -16,7 +16,7 @@ namespace GvanimVS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            
         }
     }
 }
