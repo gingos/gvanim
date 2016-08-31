5e2d39f01d34c7f0a2f7fa8bbb634ddb0cac4d8d
yoadwo <yoadwo@ac.sce.ac.il>
2016-08-31 19:52:28 +0300

    selectMitmoded edit was searching by drop down of all mitmoded, working on changing to search by params

--- GvanimVS/GvanimVS/SelectMimoded.Designer.cs
+++ GvanimVS/GvanimVS/SelectMimoded.Designer.cs
@@ -29,8 +29,14 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ok_bt = new System.Windows.Forms.Button();
             this.close_bt = new System.Windows.Forms.Button();
+            this.ID_lb = new System.Windows.Forms.Label();
+            this.name_lb = new System.Windows.Forms.Label();
+            this.city_lb = new System.Windows.Forms.Label();
+            this.search_bt = new System.Windows.Forms.Button();
+            this.ID_tb = new System.Windows.Forms.TextBox();
+            this.name_tb = new System.Windows.Forms.TextBox();
+            this.city_tb = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,22 +47,13 @@
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 62);
+            this.dataGridView1.Location = new System.Drawing.Point(40, 123);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.dataGridView1.Size = new System.Drawing.Size(463, 225);
+            this.dataGridView1.Size = new System.Drawing.Size(463, 164);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-            // 
-            // ok_bt
-            // 
-            this.ok_bt.Location = new System.Drawing.Point(119, 302);
-            this.ok_bt.Name = "ok_bt";
-            this.ok_bt.Size = new System.Drawing.Size(75, 23);
-            this.ok_bt.TabIndex = 1;
-            this.ok_bt.Text = "בחר";
-            this.ok_bt.UseVisualStyleBackColor = true;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
             // 
             // close_bt
             // 
@@ -68,25 +65,98 @@
             this.close_bt.UseVisualStyleBackColor = true;
             this.close_bt.Click += new System.EventHandler(this.close_bt_Click);
             // 
+            // ID_lb
+            // 
+            this.ID_lb.AutoSize = true;
+            this.ID_lb.Location = new System.Drawing.Point(436, 24);
+            this.ID_lb.Name = "ID_lb";
+            this.ID_lb.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ID_lb.Size = new System.Drawing.Size(67, 13);
+            this.ID_lb.TabIndex = 3;
+            this.ID_lb.Text = "תעודת זהות";
+            // 
+            // name_lb
+            // 
+            this.name_lb.AutoSize = true;
+            this.name_lb.Location = new System.Drawing.Point(480, 50);
+            this.name_lb.Name = "name_lb";
+            this.name_lb.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.name_lb.Size = new System.Drawing.Size(23, 13);
+            this.name_lb.TabIndex = 4;
+            this.name_lb.Text = "שם";
+            // 
+            // city_lb
+            // 
+            this.city_lb.AutoSize = true;
+            this.city_lb.Location = new System.Drawing.Point(478, 81);
+            this.city_lb.Name = "city_lb";
+            this.city_lb.Size = new System.Drawing.Size(25, 13);
+            this.city_lb.TabIndex = 5;
+            this.city_lb.Text = "עיר";
+            // 
+            // search_bt
+            // 
+            this.search_bt.Location = new System.Drawing.Point(428, 302);
+            this.search_bt.Name = "search_bt";
+            this.search_bt.Size = new System.Drawing.Size(75, 23);
+            this.search_bt.TabIndex = 6;
+            this.search_bt.Text = "חיפוש";
+            this.search_bt.UseVisualStyleBackColor = true;
+            this.search_bt.Click += new System.EventHandler(this.search_bt_Click);
+            // 
+            // ID_tb
+            // 
+            this.ID_tb.Location = new System.Drawing.Point(278, 24);
+            this.ID_tb.Name = "ID_tb";
+            this.ID_tb.Size = new System.Drawing.Size(134, 20);
+            this.ID_tb.TabIndex = 7;
+            // 
+            // name_tb
+            // 
+            this.name_tb.Location = new System.Drawing.Point(278, 52);
+            this.name_tb.Name = "name_tb";
+            this.name_tb.Size = new System.Drawing.Size(134, 20);
+            this.name_tb.TabIndex = 8;
+            // 
+            // city_tb
+            // 
+            this.city_tb.Location = new System.Drawing.Point(278, 81);
+            this.city_tb.Name = "city_tb";
+            this.city_tb.Size = new System.Drawing.Size(134, 20);
+            this.city_tb.TabIndex = 9;
+            // 
             // SelectMimoded
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 353);
+            this.Controls.Add(this.city_tb);
+            this.Controls.Add(this.name_tb);
+            this.Controls.Add(this.ID_tb);
+            this.Controls.Add(this.search_bt);
+            this.Controls.Add(this.city_lb);
+            this.Controls.Add(this.name_lb);
+            this.Controls.Add(this.ID_lb);
             this.Controls.Add(this.close_bt);
-            this.Controls.Add(this.ok_bt);
             this.Controls.Add(this.dataGridView1);
             this.Name = "SelectMimoded";
             this.Text = "SelectMimoded";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button ok_bt;
         private System.Windows.Forms.Button close_bt;
+        private System.Windows.Forms.Label ID_lb;
+        private System.Windows.Forms.Label name_lb;
+        private System.Windows.Forms.Label city_lb;
+        private System.Windows.Forms.Button search_bt;
+        private System.Windows.Forms.TextBox ID_tb;
+        private System.Windows.Forms.TextBox name_tb;
+        private System.Windows.Forms.TextBox city_tb;
     }
 }
--- GvanimVS/GvanimVS/SelectMimoded.cs
+++ GvanimVS/GvanimVS/SelectMimoded.cs
@@ -21,7 +21,7 @@ namespace GvanimVS
         public SelectMimoded(SqlConnection con, string type):base(con)
         {
             InitializeComponent();
-            DataTable dt = SQLmethods.getColsFromTable(SQLmethods.MITMODED, "ID, firstName, lastName", cmd, da);
+            DataTable dt = SQLmethods.getColsFromTable(SQLmethods.MITMODED, "ID, firstName, lastName, city", cmd, da);
             dataGridView1.DataSource = dt;
             changeDataHeadersToHebrew();
             this.type = type;
@@ -33,10 +33,11 @@ namespace GvanimVS
             this.Close();
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             string ID = dataGridView1["ID", e.RowIndex].Value.ToString();
-            Console.WriteLine("SelectMitmoded-->click--> ID = " +ID);
+            Console.WriteLine("SelectMitmoded-->click--> ID = " + ID);
             this.Hide();
             switch (type)
             {
@@ -58,6 +59,74 @@ namespace GvanimVS
             dataGridView1.Columns["ID"].HeaderCell.Value = "תעודת זהות";
             dataGridView1.Columns["firstName"].HeaderCell.Value = "שם פרטי";
             dataGridView1.Columns["lastName"].HeaderCell.Value = "שם משפחה";
+        }
+
+        private void search_bt_Click(object sender, EventArgs e)
+        {
+            if (verifyFields())
+            {
+
+            }
+        }
+        private bool verifyFields()
+        {
+            bool nameEmpty = false, cityEmpty = false;
+
+            if (ID_tb.Text.Equals(""))
+            {
+                if (name_tb.Text.Equals(""))
+                {
+                    DialogResult dialogResult = MessageBox.Show("לא נבחר שם לחיפוש. האם ברצונך להמשיך?", "אישור בחירת שם", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        nameEmpty = true;
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        name_tb.Focus();
+                        return false;
+                    }
+                }
+                else if (!Tools.IsAlphabets(name_tb.Text.ToString()))
+                {
+                    MessageBox.Show("נא להכניס שם תקין");
+                    name_tb.Focus();
+                    return false;
+                }
+
+                if (city_tb.Text.Equals(""))
+                {
+                    DialogResult dialogResult = MessageBox.Show("לא נבחרה עיר לחיפוש. האם ברצונך להמשיך?", "אישור בחירת עיר", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        cityEmpty = true;
+                        if (cityEmpty && nameEmpty)
+                        {
+                            MessageBox.Show("יש להזין לפחות שם או עיר");
+                            return false;
+                        }
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        city_tb.Focus();
+                        return false;
+                    }
+                }
+                else if (!Tools.IsAlphabets(city_tb.Text.ToString()))
+                {
+                    MessageBox.Show("נא להכניס שם תקין");
+                    city_tb.Focus();
+                    return false;
+                }
+            }
+            else if (!Tools.valid_number(ID_tb.Text.ToString()))
+            {
+                MessageBox.Show("נא להכניס תעודת זהות תקינה");
+                ID_tb.Focus();
+                return false;
+            }
+
+            return true;
         }
     }
 }
