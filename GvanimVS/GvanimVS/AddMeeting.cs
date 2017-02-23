c2df508480f7eef4b5748cd5d17a7a78e01633e6
yoadw20 <yoadw20@gmail.com>
2017-02-23 22:07:50 +0200

    add meeting: merged time and date pickers. maybe obsolete class?

--- GvanimVS/GvanimVS/AddMeeting.cs
+++ GvanimVS/GvanimVS/AddMeeting.cs
@@ -23,5 +23,30 @@ namespace GvanimVS
         {
             this.Close();
         }
+
+        private void addMeeting_bt_Click(object sender, EventArgs e)
+        {
+            DateTime dt = new DateTime(datePicker.Value.Year, datePicker.Value.Month, datePicker.Value.Day,
+                timePicker.Value.Hour, timePicker.Value.Minute, timePicker.Value.Second);
+            if (isValid()){
+                MessageBox.Show("valid");
+            }
+
+        }
+
+        private bool isValid()
+        {
+            if (!datePicker.Checked)
+            {
+                MessageBox.Show("נא לאשר את תאריך הפגישה");
+                return false;
+            }
+            if (!timePicker.Checked)
+            {
+                MessageBox.Show("נא לאשר את שעת הפגישה");
+                return false;
+            }
+            return true;
+        }
     }
 }
